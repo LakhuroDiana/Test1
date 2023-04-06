1a8c6b8ccd3b3d0aa47b3a0cb79448b6ce58684a
Diana <kat.lahuro.3223@gmail.com>
2023-04-06 22:37:49 +0300

    ++

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ for (int i = 0; i < initial_array.Length; ++i)
 {
     if ((initial_array[i].Length < 3) || (initial_array[i].Length == 3))
     {
-        number_of_rows_in_the_second_array += 1;
+        number_of_rows_in_the_second_array++;
     }
 }
 
