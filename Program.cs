9bc78acdd18af2ac67937c99b722aa95e9bf4021
Diana <kat.lahuro.3223@gmail.com>
2023-04-06 21:01:35 +0300

    заполнение второго массива

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
 ﻿string[] initial_array = { "hello", "2", "world", ":-)" };
-int number_of_rows_in_the_second_array=0;
+int number_of_rows_in_the_second_array = 0;
 
+//подсчёт слов, длина которых меньше или равна 3 
 Console.WriteLine(initial_array.Length);
-for (int i=0; i<initial_array.Length; ++i)
+for (int i = 0; i < initial_array.Length; ++i)
 {
-    if ((initial_array[i].Length<3) || (initial_array[i].Length==3))
+    if ((initial_array[i].Length < 3) || (initial_array[i].Length == 3))
     {
-        number_of_rows_in_the_second_array+=1;
+        number_of_rows_in_the_second_array += 1;
+    }
+}
+
+string[] modified_array = new string[number_of_rows_in_the_second_array];
+
+// запись этих слов в новый массив
+int count = 0;
+for (int i = 0; i < initial_array.Length; i++)
+{
+    if (initial_array[i].Length <= 3)
+    {
+        modified_array[count] = initial_array[i];
+        count++;
     }
 }
