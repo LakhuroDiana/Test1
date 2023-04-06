f8bb267271ec1d8bf371b2babc4ac606508ca848
Diana <kat.lahuro.3223@gmail.com>
2023-04-06 20:11:31 +0300

    подсчёт слов

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string[] initial_array = { "hello", "2", "world", ":-)" };
+int number_of_rows_in_the_second_array=0;
+
+Console.WriteLine(initial_array.Length);
+for (int i=0; i<initial_array.Length; ++i)
+{
+    if ((initial_array[i].Length<3) || (initial_array[i].Length==3))
+    {
+        number_of_rows_in_the_second_array+=1;
+    }
+}
